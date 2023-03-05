ae772daa766981e01ace861e988f918602f0da71
muhammadayaz123890 <119504188+muhammadayaz123890@users.noreply.github.com>
2023-03-05 14:07:54 +0500

    Update settings.py

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6dx+!ppo4^z8i9_v8dk(^_*c^gigm8to2*%$$(!r*k2*=5aru+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' , 'ayaz-commerce-site.herokuapp.com']
 
